d44cfe3f1a77fc4f07d2b0e36001ad03b638ae47
2431380yasamingol <yasamingol>
2023-11-13 08:23:55 -0800

    Implement Rectangle class.
    All needed fields and functionalities are implemented.

--- src/main/java/org/example/Rectangle.java
+++ src/main/java/org/example/Rectangle.java
@@ -1,4 +1,31 @@
 package main.java.org.example;
 
 public class Rectangle {
+    private int height;
+    private int width;
+
+    public Rectangle(int initialHeight, int initialWidth) {
+        height = initialHeight;
+        width = initialWidth;
+    }
+
+    public int calculateArea() {
+        return height * width;
+    }
+
+    public void setHeight(int newHeight) {
+        height = newHeight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setWidth(int newWidth) {
+        width = newWidth;
+    }
+
+    public int getWidth() {
+        return width;
+    }
 }
--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -1,5 +1,6 @@
 package test.java;
 
+import main.java.org.example.Rectangle;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
