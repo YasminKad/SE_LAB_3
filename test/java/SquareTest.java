585ac308aa5cecb88e7e012db8da0b73c6a770d0
yasminkadkhodaei <59218135+yasminkadkhodaei@users.noreply.github.com>
2023-11-13 19:30:26 +0330

    Implement the testCalculateArea function

--- test/java/SquareTest.java
+++ test/java/SquareTest.java
@@ -3,8 +3,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SquareTest {
     @Test
-    public void testCalculateArea() {}
+    public void testCalculateArea() {
+        Square square = new Square(5);
+        assertEquals(25, square.calculateArea());
+    }
 
     @Test
-    public void testGetSetSide() {}
+    public void testGetSetSide() {
+
+    }
 }
