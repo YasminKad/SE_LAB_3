f8e4cdd742cb09df55827330b0c8ee19c71ff5bc
yasminkadkhodaei <59218135+yasminkadkhodaei@users.noreply.github.com>
2023-11-13 19:26:26 +0330

    Add SquareTest Class tests

--- test/java/SquareTest.java
+++ test/java/SquareTest.java
@@ -1,2 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SquareTest {
+    @Test
+    public void testCalculateArea() {}
+
+    @Test
+    public void testGetSetSide() {}
 }
