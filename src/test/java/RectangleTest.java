7a50ae9890e92f6bc618fa860fcb4605b0ca061c
2431380yasamingol <yasamingol>
2023-11-13 08:01:02 -0800

    Add src package.

---
+++ src/test/java/RectangleTest.java
@@ -0,0 +1,15 @@
+package test.java;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RectangleTest {
+    @Test
+    public void testCalculateArea() {}
+}
+    @Test
+    public void testGetSetHeight() {}
+
+    @Test
+    public void testGetSetWidth() {
+}
